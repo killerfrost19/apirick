4bf069c18a9129a9350d63243c215986a9983e52
killerfrost19 <varshacheela19@gmail.com>
2024-01-21 17:18:32 +0530

    first

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL="https://rickandmortyapi.com/api";
 
- export const fetchCharacter= async(url)=>{
+ export const fetchCharacter= async(url)=> {
     try{
        const response=await axios.get(`${BASE_URL}${url}`);
        return response.data;    
